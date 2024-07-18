acfbfb85a6371a1890ac100158d45572b67e99b2
jordan <jordan.lesich@gmail.com>
2024-07-17 17:34:03 -0700

    allocated, distributed, gameActive

--- src/GameManagerStrategy.ts
+++ src/GameManagerStrategy.ts
@@ -1,4 +1,9 @@
-import { GameManagerStrategyContract, gameRoundLoaderConfig } from 'generated';
+import {
+  GameManagerStrategyContract,
+  gameManagerEntity,
+  gameRoundEntity,
+  gameRoundLoaderConfig,
+} from 'generated';
 import { GameStatus } from './utils/statuses';
 
 GameManagerStrategyContract.GameManagerInitialized.loader(() => {});
@@ -130,4 +135,150 @@ GameManagerStrategyContract.RecipientAccepted.handler(({ event, context }) => {
     approvedTime: event.blockTimestamp,
     applicationReviewReason_id: event.params.reason[1],
   });
+});
+
+GameManagerStrategyContract.ShipLaunched.loader(({ event, context }) => {
+  context.GrantShip.load(event.params.recipientId, {});
+});
+
+GameManagerStrategyContract.ShipLaunched.handler(({ event, context }) => {
+  const ship = context.GrantShip.get(event.params.recipientId);
+
+  if (!ship) {
+    context.log.error(`Ship not found for address ${event.params.recipientId}`);
+    return;
+  }
+
+  context.GrantShip.set({
+    ...ship,
+    shipLaunched: true,
+  });
+});
+
+GameManagerStrategyContract.Allocated.loader(({ event, context }) => {
+  context.GrantShip.load(event.params.recipientId, {});
+  context.GameManager.load(event.srcAddress, { loadCurrentRound: {} });
+});
+
+GameManagerStrategyContract.Allocated.handler(({ event, context }) => {
+  const ship = context.GrantShip.get(event.params.recipientId);
+  const gameManager = context.GameManager.get(event.srcAddress);
+
+  if (!ship) {
+    context.log.error(`Ship not found for address ${event.params.recipientId}`);
+    return;
+  }
+
+  if (!gameManager) {
+    context.log.error(`GameManager not found for address ${event.srcAddress}`);
+    return;
+  }
+  const currentRound = context.GameManager.getCurrentRound(gameManager);
+
+  if (!currentRound) {
+    context.log.error(
+      `Current round not found for GameManager ${gameManager.id}`
+    );
+    return;
+  }
+
+  context.GrantShip.set({
+    ...ship,
+    isAllocated: true,
+    status: GameStatus.Allocated,
+    shipAllocation: event.params.amount,
+  });
+
+  context.GameRound.set({
+    ...currentRound,
+    gameStatus: GameStatus.Allocated,
+    totalAllocatedAmount:
+      currentRound.totalAllocatedAmount + event.params.amount,
+  });
+});
+
+GameManagerStrategyContract.Distributed.loader(({ event, context }) => {
+  context.GrantShip.load(event.params.recipientId, {});
+  context.GameManager.load(event.srcAddress, { loadCurrentRound: {} });
+});
+
+GameManagerStrategyContract.Distributed.handler(({ event, context }) => {
+  const ship = context.GrantShip.get(event.params.recipientId);
+  const gameManager = context.GameManager.get(event.srcAddress);
+
+  if (!ship) {
+    context.log.error(`Ship not found for address ${event.params.recipientId}`);
+    return;
+  }
+
+  if (!gameManager) {
+    context.log.error(`GameManager not found for address ${event.srcAddress}`);
+    return;
+  }
+
+  const currentRound = context.GameManager.getCurrentRound(gameManager);
+
+  if (!currentRound) {
+    context.log.error(
+      `Current round not found for GameManager ${gameManager.id}`
+    );
+    return;
+  }
+
+  context.GrantShip.set({
+    ...ship,
+    isDistributed: true,
+    status: GameStatus.Active,
+    totalRoundAmount: event.params.grantAmount,
+  });
+
+  context.GameRound.set({
+    ...currentRound,
+    gameStatus: GameStatus.Funded,
+    totalDistributedAmount:
+      currentRound.totalDistributedAmount + event.params.grantAmount,
+  });
+});
+
+GameManagerStrategyContract.GameActive.loader(({ event, context }) => {
+  context.GameManager.load(event.srcAddress, { loadCurrentRound: {} });
+});
+
+GameManagerStrategyContract.GameActive.handler(({ event, context }) => {
+  const gameManager = context.GameManager.get(event.srcAddress);
+
+  if (!gameManager) {
+    context.log.error(`GameManager not found for address ${event.srcAddress}`);
+    return;
+  }
+
+  const currentRound = context.GameManager.getCurrentRound(gameManager);
+
+  if (!currentRound) {
+    context.log.error(
+      `Current round not found for GameManager ${gameManager.id}`
+    );
+    return;
+  }
+
+  if (event.params.active === true) {
+    context.GameRound.set({
+      ...currentRound,
+      gameStatus: GameStatus.Active,
+      isGameActive: true,
+      realStartTime: event.blockTimestamp,
+    });
+  } else {
+    context.GameRound.set({
+      ...currentRound,
+      gameStatus: GameStatus.Active,
+      isGameActive: false,
+      realStartTime: event.blockTimestamp,
+    });
+
+    context.GameManager.set({
+      ...gameManager,
+      currentRound_id: `${event.srcAddress}-${event.params.gameIndex}`,
+    });
+  }
 });
