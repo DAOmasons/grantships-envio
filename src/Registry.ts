498d38561fdcc0acb2a9012bc79f52f0b9bd2ca5
jordan <jordan.lesich@gmail.com>
2024-11-11 16:32:53 -0800

    registry, allo poster resoloved

--- src/Registry.ts
+++ src/Registry.ts
@@ -1,134 +1,134 @@
-// import { Registry } from 'generated';
-// import { GameStatus } from './utils/constants';
-// import { addTransaction } from './utils/sync';
+import { Registry } from 'generated';
+import { GameStatus } from './utils/constants';
+import { addTransaction } from './utils/sync';
 
-// Registry.ProfileCreated.handler(async ({ event, context }) => {
-//   if (event.params.metadata[0] == 103115010001003n) {
-//     context.RawMetadata.set({
-//       id: event.params.metadata[1],
-//       protocol: event.params.metadata[0],
-//       pointer: event.params.metadata[1],
-//     });
+Registry.ProfileCreated.handler(async ({ event, context }) => {
+  if (event.params.metadata[0] == 103115010001003n) {
+    context.RawMetadata.set({
+      id: event.params.metadata[1],
+      protocol: event.params.metadata[0],
+      pointer: event.params.metadata[1],
+    });
 
-//     context.Project.set({
-//       id: event.params.anchor,
-//       chainId: event.chainId,
-//       profileId: event.params.profileId,
-//       status: GameStatus.None,
-//       nonce: event.params.nonce,
-//       name: event.params.name,
-//       metadata_id: event.params.metadata[1],
-//       owner: event.params.owner,
-//       anchor: event.params.anchor,
-//       members_id: event.params.profileId,
-//       totalAmountReceived: BigInt(0),
-//       pastProfileIds: [],
-//       pastNames: [],
-//       hasEditedProfile: false,
-//     });
+    context.Project.set({
+      id: event.params.anchor,
+      chainId: event.chainId,
+      profileId: event.params.profileId,
+      status: GameStatus.None,
+      nonce: event.params.nonce,
+      name: event.params.name,
+      metadata_id: event.params.metadata[1],
+      owner: event.params.owner,
+      anchor: event.params.anchor,
+      members_id: event.params.profileId,
+      totalAmountReceived: BigInt(0),
+      pastProfileIds: [],
+      pastNames: [],
+      hasEditedProfile: false,
+    });
 
-//     context.ProfileIdToAnchor.set({
-//       id: event.params.profileId,
-//       profileId: event.params.profileId,
-//       anchor: event.params.anchor,
-//     });
+    context.ProfileIdToAnchor.set({
+      id: event.params.profileId,
+      profileId: event.params.profileId,
+      anchor: event.params.anchor,
+    });
 
-//     addTransaction(event, context);
-//   }
-//   if (event.params.metadata[0] == 103115010001004n) {
-//     context.RawMetadata.set({
-//       id: event.params.metadata[1],
-//       protocol: event.params.metadata[0],
-//       pointer: event.params.metadata[1],
-//     });
+    addTransaction(event, context);
+  }
+  if (event.params.metadata[0] == 103115010001004n) {
+    context.RawMetadata.set({
+      id: event.params.metadata[1],
+      protocol: event.params.metadata[0],
+      pointer: event.params.metadata[1],
+    });
 
-//     context.GrantShip.set({
-//       id: event.params.anchor,
-//       profileId: event.params.profileId,
-//       chainId: event.chainId,
-//       name: event.params.name,
-//       nonce: event.params.nonce,
-//       anchor: event.params.anchor,
-//       profileMetadata_id: event.params.metadata[1],
-//       pastProfileIds: [],
-//       pastNames: [],
-//       hasEditedProfile: false,
-//       owner: event.params.owner,
-//       status: GameStatus.None,
-//       poolFunded: false,
-//       alloProfileMembers_id: event.params.profileId,
-//       balance: BigInt(0),
-//       shipAllocation: BigInt(0),
-//       totalFundsReceived: BigInt(0),
-//       totalRoundAmount: BigInt(0),
-//       totalAllocated: BigInt(0),
-//       totalDistributed: BigInt(0),
-//       gameRound_id: undefined,
-//       gameManager_id: undefined,
-//       shipApplicationBytesData: undefined,
-//       applicationSubmittedTime: undefined,
-//       isAwaitingApproval: undefined,
-//       hasSubmittedApplication: undefined,
-//       isApproved: undefined,
-//       approvedTime: undefined,
-//       isRejected: undefined,
-//       rejectedTime: undefined,
-//       applicationReviewReason_id: undefined,
-//       poolId: undefined,
-//       hatId: undefined,
-//       shipContractAddress: undefined,
-//       shipLaunched: undefined,
-//       poolActive: undefined,
-//       isAllocated: undefined,
-//       isDistributed: undefined,
-//       beaconMessage_id: undefined,
-//       customApplication_id: undefined,
-//       beaconLastUpdated: undefined,
-//     });
+    context.GrantShip.set({
+      id: event.params.anchor,
+      profileId: event.params.profileId,
+      chainId: event.chainId,
+      name: event.params.name,
+      nonce: event.params.nonce,
+      anchor: event.params.anchor,
+      profileMetadata_id: event.params.metadata[1],
+      pastProfileIds: [],
+      pastNames: [],
+      hasEditedProfile: false,
+      owner: event.params.owner,
+      status: GameStatus.None,
+      poolFunded: false,
+      alloProfileMembers_id: event.params.profileId,
+      balance: BigInt(0),
+      shipAllocation: BigInt(0),
+      totalFundsReceived: BigInt(0),
+      totalRoundAmount: BigInt(0),
+      totalAllocated: BigInt(0),
+      totalDistributed: BigInt(0),
+      gameRound_id: undefined,
+      gameManager_id: undefined,
+      shipApplicationBytesData: undefined,
+      applicationSubmittedTime: undefined,
+      isAwaitingApproval: undefined,
+      hasSubmittedApplication: undefined,
+      isApproved: undefined,
+      approvedTime: undefined,
+      isRejected: undefined,
+      rejectedTime: undefined,
+      applicationReviewReason_id: undefined,
+      poolId: undefined,
+      hatId: undefined,
+      shipContractAddress: undefined,
+      shipLaunched: undefined,
+      poolActive: undefined,
+      isAllocated: undefined,
+      isDistributed: undefined,
+      beaconMessage_id: undefined,
+      customApplication_id: undefined,
+      beaconLastUpdated: undefined,
+    });
 
-//     context.ProfileIdToAnchor.set({
-//       id: event.params.profileId,
-//       profileId: event.params.profileId,
-//       anchor: event.params.anchor,
-//     });
+    context.ProfileIdToAnchor.set({
+      id: event.params.profileId,
+      profileId: event.params.profileId,
+      anchor: event.params.anchor,
+    });
 
-//     addTransaction(event, context);
-//   }
-// });
+    addTransaction(event, context);
+  }
+});
 
-// Registry.RoleGranted.handler(async ({ event, context }) => {
-//   const profile = await context.ProfileMemberGroup.get(event.params.role);
+Registry.RoleGranted.handler(async ({ event, context }) => {
+  const profile = await context.ProfileMemberGroup.get(event.params.role);
 
-//   if (!profile) {
-//     context.ProfileMemberGroup.set({
-//       id: event.params.role,
-//       role: event.params.role,
-//       addresses: [event.params.account],
-//     });
-//   } else {
-//     context.ProfileMemberGroup.set({
-//       ...profile,
-//       addresses: [...profile.addresses, event.params.account],
-//     });
-//   }
-// });
+  if (!profile) {
+    context.ProfileMemberGroup.set({
+      id: event.params.role,
+      role: event.params.role,
+      addresses: [event.params.account],
+    });
+  } else {
+    context.ProfileMemberGroup.set({
+      ...profile,
+      addresses: [...profile.addresses, event.params.account],
+    });
+  }
+});
 
-// Registry.RoleRevoked.handler(async ({ event, context }) => {
-//   const profile = await context.ProfileMemberGroup.get(event.params.role);
+Registry.RoleRevoked.handler(async ({ event, context }) => {
+  const profile = await context.ProfileMemberGroup.get(event.params.role);
 
-//   if (!profile) {
-//     return;
-//   }
+  if (!profile) {
+    return;
+  }
 
-//   const addresses = profile.addresses.filter(
-//     (address) => address != event.params.account
-//   );
+  const addresses = profile.addresses.filter(
+    (address) => address != event.params.account
+  );
 
-//   context.ProfileMemberGroup.set({
-//     ...profile,
-//     addresses,
-//   });
-// });
+  context.ProfileMemberGroup.set({
+    ...profile,
+    addresses,
+  });
+});
 
 // Registry.ProfileMetadataUpdated.handler(async ({ event, context }) => {
 //   const profileJoin = await context.ProfileIdToAnchor.get(
